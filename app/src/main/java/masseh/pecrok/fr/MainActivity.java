b7470bfe52a817e426636c395a25abaef53e77a5
kevinpecro <kevinpecro@live.fr>
2019-02-26 18:55:19 +0100

    Création du projet

---
+++ app/src/main/java/masseh/pecrok/fr/MainActivity.java
@@ -0,0 +1,13 @@
+package masseh.pecrok.fr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
